3efc4d710d24ff8afc1c7a562a7b927c613c1b4a
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2023-08-09 20:10:05 +0300

    add code

--- 50-object-task-10.js
+++ 50-object-task-10.js
@@ -1,6 +1,7 @@
 // Ви програмуєте онлайн магазин і потрібно створити функцію, 
 // яка приймає на вхід об'єкт з інформацією про товар, включаючи назву
-// товару, його ціну та кількість на складі. Функція повинна 
+// товару, його ціну та кількість на складі. 
+// Функція повинна 
 // повертати рядок з описом товару та його ціни з подальшим 
 // застосуванням знижки, якщо така є. Якщо товару немає на складі, 
 // функція повинна повернути повідомлення про це.
@@ -18,11 +19,21 @@ const product2 = {
     stock: 80,
     discount: 10,
 };
+const product3 = {
+    name: 'Шкарпетки',
+    price: 45,
+    stock: 0,
+    discount: 0,
+};
 
-let products = {
-    name: [],
-    price: [],
-    stock: [],
-    discount: []
+function generateDescription({name,price,stock,discount}){
+    if(stock === 0){
+        return `Товару не має на складі немає`
+    }else{
+        return `${name} по ${price * (100 - discount) / 100} за штуку`
+    }
 }
 
+console.table(generateDescription(product1))
+console.table(generateDescription(product2))
+console.table(generateDescription(product3))
